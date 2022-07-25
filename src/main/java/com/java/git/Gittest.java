7a3cd0351185d9e32b7f84f33c48543461c3fcce
LiLei <LiLei@qq.com>
2022-07-25 15:09:47 +0800

    second commit

--- src/main/java/com/java/git/Gittest.java
+++ src/main/java/com/java/git/Gittest.java
@@ -3,5 +3,6 @@ package com.java.git;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
